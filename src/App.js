37945e040051221e7d0c2f5b804172071a70232d
Yarden vin <yardenvin@gmail.com>
2024-03-06 19:18:41 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
     <h1>react git pages</h1>
+    <h1>New line</h1>
     </div>
   );
 }
